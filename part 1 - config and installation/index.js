3a258396c5e71aad8f8bd127f49c6fe3bc8646a8
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2018-03-15 16:21:52 -0700

    Add files via upload

--- part 1 - config and installation/index.js
+++ part 1 - config and installation/index.js
@@ -1,1 +1,15 @@
+import express from 'express';
+//import express from express
 
+//create a constant since we will never need to change any app's values
+//plus it is es6 feature
+const app = express();
+
+//now we need to create a base server 
+app.get('/', (req, res) => {
+    res.send('GraphQL is badass!' );
+})
+
+//now we need to set up a listening port where will app / express will be listening on
+// down below we are telling the server once you come alive throw a conssole log that it is running 
+app.listen(8080, () => console.log("the server is running"));
