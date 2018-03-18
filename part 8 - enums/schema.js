79826b168e79aec4fbbf0e760047535b8131928c
CaptainStorm21 <CaptainStorm21@users.noreply.github.com>
2018-03-17 17:26:14 -0700

    Add files via upload

---
+++ part 8 - enums/schema.js
@@ -0,0 +1,45 @@
+import { buildSchema } from 'graphql'
+
+const schema = buildSchema(
+  `
+
+    type Employee {
+        id: ID
+        firstName: String
+        lastName: String
+        position: String
+        gender: Gender
+        language: String
+        emails: String
+        age: Int
+
+    }
+    enum Gender {
+        MALE
+        FEMALE
+    }
+
+
+    type Query {
+        getEmployee(id:ID): Employee
+    }
+
+    input EmployeeInput {
+        id: ID
+        firstName: String!
+        lastName: String!
+        position: String   
+        gender: Gender
+        language: String
+        email: String
+        age: Int
+    }
+
+    type Mutation {
+        createEmployee (input: EmployeeInput): Employee
+    }
+
+`
+)
+
+export default schema
